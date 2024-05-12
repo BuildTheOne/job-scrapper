7c900590c96edcbb9b10ba280f8b258edcb7c942
Ignatius Henriyanto Primai Renda <76039582+BuildTheOne@users.noreply.github.com>
2024-05-12 12:11:23 +0700

    fix prisma prod

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './AppModule';
+import prismaClient from './config/prisma';
+import { config } from 'dotenv';
 
 (async () => {
+  config({
+    path: '.env',
+  });
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(3000, async () => {
+    try {
+      await prismaClient.$connect();
+    } catch (err) {
+      await prismaClient.$disconnect();
+      throw new Error(err);
+    }
+  });
 })();

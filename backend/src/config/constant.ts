9568c245fd24367b903d07199f04fedf108f73dd
Ignatius Henriyanto Primai Renda <76039582+BuildTheOne@users.noreply.github.com>
2024-05-12 09:48:10 +0700

    change folder name to backend

---
+++ backend/src/config/constant.ts
@@ -0,0 +1,2 @@
+const linkedinUrl =
+  'https://www.linkedin.com/jobs/search?keywords=programmer&location=Indonesia&geoId=102478259&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0';

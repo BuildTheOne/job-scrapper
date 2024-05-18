e5fbccb04113d2b7b5d7afbae1599730a04d1386
Ignatius Henriyanto Primai Renda <76039582+BuildTheOne@users.noreply.github.com>
2024-05-18 13:13:41 +0700

    [UPDATE] api url to prod

--- web-client/src/app/job/actions.ts
+++ web-client/src/app/job/actions.ts
@@ -4,8 +4,6 @@ import { format } from "date-fns";
 import { Job, JobSearchQuery } from "./types";
 
 const searchJob = async (query: JobSearchQuery) => {
-  "use server";
-
   const queryTitle = query.title ? `title=${query.title}` : "";
   const queryDate = query.publicationDate
     ? `publicationDate=${format(query.publicationDate, "yyyy-MM-dd")}`
@@ -13,7 +11,7 @@ const searchJob = async (query: JobSearchQuery) => {
   const queryLocation = query.location ? `location=${query.location}` : "";
   const queryCompany = query.company ? `company=${query.company}` : "";
 
-  const url = `http://localhost:3000/api/job?${queryTitle}&${queryDate}&${queryLocation}&${queryCompany}`;
+  const url = `${process.env.API_URL}/api/job?${queryTitle}&${queryDate}&${queryLocation}&${queryCompany}`;
 
   const req = await fetch(url);
   const res = await req.json();
